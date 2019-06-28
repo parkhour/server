2f848b6e2b33d814ddd1a638e7cba864b6520c02
mar-sh <marasi.af@gmail.com>
2019-06-28 16:13:46 +0700

    setting up server

---
+++ server.js
@@ -0,0 +1,72 @@
+const chalk = require('chalk');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+const lusca = require('lusca');
+const moment = require('moment');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+dotenv.config();
+
+const app = express();
+const dbURL = process.env.ATLAS_URI || `mongodb://localhost:27017/${process.env.MONGODB_DB}-${process.env.NODE_ENV}`;
+const port = process.env.PORT || 3000;
+
+
+const mainRoute = require('./routes/web');
+
+mongoose.connect(dbURL, { useNewUrlParser: true });
+mongoose.connection.on('error', (error) => {
+  console.log(error);
+  console.log('Please make sure MongoDB is running');
+  process.exit();
+});
+
+morgan.token('date', (req, res) => { 
+  return moment(req.headers['date']).format('MMMM Do YYYY, h:mm:ss a'); 
+});
+
+morgan.token('status', (req, res) => {
+  if(res.statusCode < 400) {
+    return chalk.green.bold(res.statusCode);
+  } else {
+    return chalk.red.bold(res.statusCode);
+  };
+});
+
+const logger = morgan(function (tokens, req, res) {
+  return [
+      chalk.whiteBright(tokens.date(req, res)),
+      chalk.green.bold(tokens.method(req, res)),
+      tokens.status(req, res),
+      chalk.blue.bold(tokens.url(req, res)),
+      chalk.yellow(`${tokens['response-time'](req, res)} ms`),
+  ].join(chalk.white.bold(' - '));
+});
+
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(lusca.xframe('DENY'));
+app.use(lusca.xssProtection(true));
+app.use(lusca.nosniff(true));
+app.use(lusca.referrerPolicy('origin'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(logger);
+} else {
+  app.use(morgan('dev'));
+};
+
+app.use('/', mainRoute);
+
+app.listen(port, () => {
+  if(process.env.NODE_ENV === 'development') {
+    console.log(chalk.white(`Server is listening on port: ${chalk.white.bold(port)}\ntime: ${chalk.white.bold(moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a'))}`));
+  } else {
+    console.log('Server is running on port', port);
+  };
+});
